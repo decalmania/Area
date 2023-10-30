cb99eb29db05476c7025fbfcbc9116b519ca43a9
Usuario <Usuario@INVENT081>
2023-10-30 10:38:28 -0300

    Adicionar arquivos de projeto.

---
+++ area/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double A = Convert.ToDouble(Console.ReadLine());
+        double B = Convert.ToDouble(Console.ReadLine());
+        double C = Convert.ToDouble(Console.ReadLine());
+        const double pi = 3.141;
+
+        double areaTriangulo = A * C;
+        double areaCirculo = (C * C) * pi;
+        double areaTrapezio = ((A + B) * C) / 2;
+        double areaQuadrado = B * B;
+        double areaRetangulo = A * B;
+
+        Console.WriteLine("TRIANGULO: " + areaTriangulo);
+        Console.WriteLine("CIRCULO: " + areaCirculo);
+        Console.WriteLine("TRAPEZIO: " + areaTrapezio);
+        Console.WriteLine("QUADRADO: " + areaQuadrado);
+        Console.WriteLine("RETANGULO: " + areaRetangulo);
+    }
+
+}
